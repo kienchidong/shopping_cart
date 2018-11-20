c28a5167faac4ae70711d570b4ac0153d7c632eb
kienc <kienchidong>
2018-11-20 19:59:51 +0700

    unit

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     session_start();
     require_once ('template/database.php');
     $database = new Database();
+    $tong=0;
 ?>
 
 
@@ -15,7 +16,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
 
 </head>
 <body>
@@ -29,33 +30,48 @@
             <th>Tên sản phẩm</th>
             <th>Hình ảnh</th>
             <th>Giá tiền</th>
+            <th>Số lượng</th>
+            <th>Thành tiền</th>
+            <th>Action</th>
         </tr>
         </thead>
         <tbody>
-        <?php
-            $sql="SELECT * FROM products";
-            $products = $database->runQuery($sql);
 
-        ?>
-        <?php if ($products) :?>
+        <?php if ($_SESSION) :?>
 
-            <?php foreach ($products as $product) :
-
+            <?php foreach ($_SESSION['dulieu'] as $key => $cart) :
+                $tong= $tong + $cart['price'] * $cart['quantity']
 
                 ?>
                 <tr>
-                    <td><?php echo $product['id'] ?></td>
-                    <td><?php echo $product['product_name'] ?></td>
-                    <td><?php echo $product['product_image'] ?></td>
-                    <td><?php echo number_format($product['price'],0)." VND" ?></td>
+                    <td><?php echo $cart['id'] ?></td>
+                    <td><?php echo $cart['product_name'] ?></td>
+                    <td><?php echo $cart['product_image'] ?></td>
+                    <td><?php echo number_format($cart['price'],0)." VND" ?></td>
+                    <td><?php echo $cart['quantity'] ?></td>
+                    <td><?php echo number_format(($cart['price'] * $cart['quantity']),0)." VND" ?></td>
+                    <td>
+                        <form name="remove<?php echo $cart['id']; ?>" action="process.php" method="post">
+                            <input type="hidden" name="id" value="<?php echo $cart['id'];?>">
+                            <input type="hidden" name="action" value="delete">
+                            <input  type="submit" name="xoa" class="btn btn-sm btn-outline-secondary xoa" value="Xóa">
+                        </form>
+                    </td>
                 </tr>
+
 
             <?php endforeach; ?>
         <?php endif; ?>
         </tbody>
     </table>
     <div>
-        Tổng hóa đơn thành toán<strong>240000</strong>
+        Tổng hóa đơn thành toán: <strong><?php echo number_format($tong, 0) ." VND"; ?></strong>
+    </div>
+    <div>
+        <form name="remove<?php echo $cart['id']; ?>" action="process.php" method="post">
+            <input type="hidden" name="action" value="thanhtoan">
+            <input  type="submit" name="thanhtoan" class="btn btn-sm btn-outline-secondary" value="Tiến hành thanh toán">
+        </form>
     </div>
     <div class="row">
         <?php
@@ -68,14 +84,16 @@
             <?php foreach ($products as $product) :?>
             <div class="col-sm-4">
                 <div class="hang">
-                    <form name="<?php echo $product['product_name'];?>" action="" method="post">
+                    <form name="<?php echo $product['product_name'];?>" action="process.php" method="post">
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" style="height: 315px; width: 100%; display: block;" src="images/products/<?php echo $product['product_image'];?>" data-holder-rendered="true" title="<?php echo '  Giá: '.number_format($product['price'],0)." VND"; ?>">                    <div class="card-body">
                             <p class="card-text" style="font-weight: bold"><?php echo $product['product_name'];?></p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="form-inline">
-                                    <input type="number" class="form-control" name="quantity" value="1">
-                                    <input  type="submit" name="submit" value="Thêm vào giỏ hàng">
+                                    <input type="number" class="form-control" name="quantity" value="1"><br>
+                                    <input type="hidden" name="action" value="add">
+                                    <input type="hidden" name="id" value="<?php echo $product['id'];?>">
+                                    <input  type="submit" name="submit" class="btn btn-sm btn-outline-secondary" value="Thêm vào giỏ hàng"><i class="fas fa-plus-square"></i>
                                 </div>
                             </div>
                         </div>
--- process.php
+++ process.php
@@ -1,2 +1,76 @@
 <?php
     session_start();
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+    require_once ('template/database.php');
+    $db= new Database();
+    if(isset($_POST['action'])){
+        switch (($_POST['action'])){
+            case 'add':
+                {
+                    $sql = "SELECT * FROM products WHERE id=" . $_POST['id'];
+                    $data = $db->runQuery($sql);
+                    $data = current($data);
+                    if (isset($_SESSION['dulieu']) && !empty($_SESSION['dulieu'])) {
+                        $product_id = $data['id'];
+                        if (isset($_SESSION['dulieu'][$product_id])) {
+                            $exist_dulieu = $_SESSION['dulieu'][$product_id];
+                            $exist_quantity = $exist_dulieu['quantity'];
+                            $dulieu = array();
+                            $dulieu['id'] = $data['id'];
+                            $dulieu['product_name'] = $data['product_name'];
+                            $dulieu['product_image'] = $data['product_image'];
+                            $dulieu['price'] = $data['price'];
+                            $dulieu['quantity'] = $exist_quantity + $_POST['quantity'];
+                            $_SESSION['dulieu'][$product_id] = $dulieu;
+                        } else {
+
+                            $product_id = $data['id'];
+                            $dulieu = array();
+                            $dulieu['id'] = $data['id'];
+                            $dulieu['product_name'] = $data['product_name'];
+                            $dulieu['product_image'] = $data['product_image'];
+                            $dulieu['price'] = $data['price'];
+                            $dulieu['quantity'] = $_POST['quantity'];
+                            $_SESSION['dulieu'][$product_id] = $dulieu;
+                        }
+
+
+                    } else {
+                        $_SESSION['dulieu'] = array();
+                        $product_id = $data['id'];
+                        $dulieu = array();
+                        $dulieu['id'] = $data['id'];
+                        $dulieu['product_name'] = $data['product_name'];
+                        $dulieu['product_image'] = $data['product_image'];
+                        $dulieu['price'] = $data['price'];
+                        $dulieu['quantity'] = $_POST['quantity'];
+                        $_SESSION['dulieu'][$product_id] = $dulieu;
+                    }
+                    break;
+                }
+            case 'delete':
+                {
+                    if(isset($_POST['id']))
+                    {
+                        $id=$_POST['id'];
+                        if(isset($_SESSION['dulieu'][$id])){
+                            unset($_SESSION['dulieu'][$id]);
+                        }
+                    }
+                    break;
+                }
+            case 'thanhtoan':
+                {
+                    session_destroy();
+                    break;
+                }
+
+        }
+    }
+
+    echo 'data';
+
+    header('Location: http://localhost:8080/shopping_cart/index.php');
+die;
